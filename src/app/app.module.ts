b6a022e7867c507fd3fb53b06a24a8e467c8642f
Arun Reddy <arun.reddy143@gmail.com>
2021-06-07 12:27:03 +0100

    FS-V1 - Setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,13 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
+// Material module
+import { MaterialModule } from 'src/app/dashboard/material.module';
+
+// Project Components
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -38,7 +45,10 @@ import { ReportsComponent } from './dashboard/reports/reports.component';
     ReactiveFormsModule,
     MaterialModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {provide:LocationStrategy, useClass : HashLocationStrategy}
+  ],
+  bootstrap: [AppComponent],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
